5d4d6d5f79f541671ff27841b5c2a0f6baf87370
mark bashir <beejayphil@gmail.com>
2020-01-09 00:17:24 +0100

    cleaned up app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
-import { GifService } from 'src/app/services/gif-service.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gif-app';
-  gifs: Array<any>;
-  constructor(private gifService: GifService) { 
-
-  }
-  searchText(search) {
-    this.gifService.getGifs(search).subscribe(gifs => { 
-      this.gifs = gifs.data;
-    })
+  constructor() {
   }
 
 
