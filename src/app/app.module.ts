e2291dcbe1c1ed59d2b0259bb12156294e00a737
mark bashir <beejayphil@gmail.com>
2020-01-08 20:23:54 +0100

    added Http client module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFieldComponent } from './search-field/search-field.component';
 import { ResultsListComponent } from './results-list/results-list.component';
+import { GifCardComponent } from './gif-card/gif-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFieldComponent,
-    ResultsListComponent
+    ResultsListComponent,
+    GifCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
