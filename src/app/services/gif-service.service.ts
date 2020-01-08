8cfcf294b9fe146592fda4f77a02dab3fc5601d1
mark bashir <beejayphil@gmail.com>
2020-01-08 20:29:10 +0100

    added url API end point

--- src/app/services/gif-service.service.ts
+++ src/app/services/gif-service.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 const httpOptions = {
     headers: new HttpHeaders({
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
     })
 };
 
@@ -14,5 +14,13 @@ export class GifServiceService {
   url: string;
   constructor(private httpClient: HttpClient) {
     
-   }
+  }
+  getGifs(query: string): Observable<any> {
+
+   return this.httpClient.get(
+      `https://api.giphy.com/v1/gifs/search?api_key=G62Z8yE5XezI46BaSodbhu730Uf7AL1u&q=${query}&limit=25&offset=0&rating=G&lang=en`,
+      httpOptions
+    );
+
+  }
 }
