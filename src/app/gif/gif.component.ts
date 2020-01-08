60d4f24ec674603194039eb3586aa745855fa187
mark bashir <beejayphil@gmail.com>
2020-01-09 00:17:04 +0100

    added gif service to make http request to API

--- src/app/gif/gif.component.ts
+++ src/app/gif/gif.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { GifService } from '../services/gif-service.service';
+
 
 @Component({
   selector: 'app-gif',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gif.component.css']
 })
 export class GifComponent implements OnInit {
-
-  constructor() { }
+  id: string;
+  gif: any;
+  constructor(private route: ActivatedRoute,private gifService: GifService) { }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => { 
+      this.id = params.get("id");
+      this.gifService.getGifById(this.id).subscribe(gif => { 
+        this.gif = gif.data;
+
+      });
+    });
   }
 
 }
