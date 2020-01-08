19833b36e3cae7441ddaa041907de6eee060a858
mark bashir <beejayphil@gmail.com>
2020-01-08 20:51:21 +0100

    added id property

--- src/app/gif-card/gif-card.component.ts
+++ src/app/gif-card/gif-card.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit,Input } from '@angular/core';
 export class GifCardComponent implements OnInit {
   @Input() title: string;
   @Input() imageUrl: string;
+  @Input() id: string;
   constructor() { }
 
   ngOnInit() {
