287053e381c71584cfb0a2cd71e2d214a1b6b563
mark bashir <beejayphil@gmail.com>
2020-01-09 00:15:58 +0100

    added a home component and route for it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GifComponent } from './gif/gif.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [{path:"gif/:id",component:GifComponent}];
+const routes: Routes = [{path:"",component:HomeComponent},{path:'gifs/:id',component:GifComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
