ae901dac69bc607a44832409e655b735c8b41861
mark bashir <beejayphil@gmail.com>
2020-01-08 20:16:24 +0100

    added gif card

---
+++ src/app/gif-card/gif-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-card',
+  templateUrl: './gif-card.component.html',
+  styleUrls: ['./gif-card.component.css']
+})
+export class GifCardComponent implements OnInit {
+  @Input() title: string;
+  @Input() imageUrl: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
